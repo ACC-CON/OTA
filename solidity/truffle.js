6bd72ce900bae7bccf74910b962563a26c2c51fc
Zhang Zhanpeng <zhangregister@outlook.com>
2021-03-15 02:48:29 +0800

    Deployed contracts

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -3,11 +3,9 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      from: '0XDeployer_ETH_Address',
-      gas: 4612388
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777", // Match any network id
     }
   }
 };
